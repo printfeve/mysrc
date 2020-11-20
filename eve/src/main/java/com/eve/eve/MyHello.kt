877fb129c024a8db20761f5ccf65595a5bd2a11d
chenwenwei1 <chenwenwei1@yy.com>
2020-11-20 17:32:43 +0800

    eve

---
+++ eve/src/main/java/com/eve/eve/MyHello.kt
@@ -0,0 +1,15 @@
+package com.eve.eve
+
+import android.util.Log
+
+/**
+ * Author: chenwenwei
+ * Date: 2020/11/20
+ * Desc:
+ * SinceVer:
+ */
+class MyHello {
+    fun say() {
+        Log.i("eve_eve", "hello_eve")
+    }
+}
